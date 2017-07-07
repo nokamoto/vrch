fe8a4795a1aa63341ec1bcc17561ad2f58ceb7cc
nokamoto <nokamoto@hotmail.co.jp>
2017-07-07 22:58:14 +0900

    Do not respond to system messages

--- slackbridge/src/main/scala/vrch/slackbridge/slack/RtmMessage.scala
+++ slackbridge/src/main/scala/vrch/slackbridge/slack/RtmMessage.scala
@@ -2,7 +2,7 @@ package vrch.slackbridge.slack
 
 import play.api.libs.json.{Json, OFormat}
 
-case class RtmMessage(user: String, channel: String, text: String)
+case class RtmMessage(user: String, channel: String, text: String, subtype: Option[String])
 
 object RtmMessage {
   implicit val format: OFormat[RtmMessage] = Json.format
