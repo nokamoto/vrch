9875636273fa67d83ca27e3277ab66c5ba474d05
nokamoto <nokamoto@hotmail.co.jp>
2017-07-17 23:51:59 +0900

    Authenticate as anonymous user

--- VrchAndroid/app/src/main/java/nokamoto/github/com/vrchandroid/AccountPreference.java
+++ VrchAndroid/app/src/main/java/nokamoto/github/com/vrchandroid/AccountPreference.java
@@ -1,7 +1,10 @@
 package nokamoto.github.com.vrchandroid;
 
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
+
+import com.google.firebase.auth.FirebaseUser;
 
 public class AccountPreference {
     private Context context;
@@ -14,5 +17,20 @@ public class AccountPreference {
         String key = context.getResources().getString(R.string.pref_account_key_display_name);
         String defaultValue = context.getResources().getString(R.string.pref_account_default_display_name);
         return PreferenceManager.getDefaultSharedPreferences(context).getString(key, defaultValue);
+    }
+
+    void setDisplayName(String value) {
+        String key = context.getResources().getString(R.string.pref_account_key_display_name);
+        SharedPreferences.Editor editor = PreferenceManager.getDefaultSharedPreferences(context).edit();
+        editor.putString(key, value);
+        editor.apply();
+    }
+
+    String uid(FirebaseUser user) {
+        try {
+            return user.getUid();
+        } catch (NullPointerException e) {
+            return context.getResources().getString(R.string.pref_account_default_uid);
+        }
     }
 }
